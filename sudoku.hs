6dc27231298e7aa0ba67ed1e29492c0396f01bb6
Hieu Do Ngoc <hieudn.uh@gmail.com>
2019-05-23 17:44:43 -0400

    Minor code fixes, Add getEmptyCells and getOccupiedCells

--- sudoku.hs
+++ sudoku.hs
@@ -4,15 +4,16 @@ module Sudoku where
 
 	data Nonomino = Nonomino [(Int, Int)]
 
-	------------------------------ Funciones ---------------------------
+	data GeoSudoku = GeoSudoku [((Int, Int), Nonomino)]
 
-	origen (Nonomino (h:t)) = h
+	------------------------------ Funciones ---------------------------
 
 	-- Devuelve la lista de tuplas (x, xs) donde x es un elemento de la lista dada
 	-- y xs es el resto
 	selections :: [a] -> [(a, [a])]
 	selections [] = []
 	selections (x:xs) = (x,xs) : [(y,x:ys) | (y,ys) <- selections xs]
+
 
 	-- Devuelve la lista de todas las posibles permutaciones de 'list'
 	permutations :: [a] -> [[a]]
@@ -24,11 +25,22 @@ module Sudoku where
 	-- Devuelve la lista de las permutaciones de los nonominos que representan
 	-- tableros validos de sudoku
 	buildSudokus :: [Nonomino] -> [[Int]]
-	buildSudokus nonos = [ list | list <- permutations [1..9], 
+	buildSudokus nonos = [ list | list <- permutations [0..8],
 											validOrder list nonos]
+
 
 	validOrder :: [Int] -> [Nonomino] -> Bool
 	validOrder perm nonos = True
 
-	getOccupied list = [(a+i, b+j) | ((a, b), Nonomino points) <- list,
-												(i,j) <- points]
+
+	-- Devuelve los pares ordenados (i, j) que estan ocupados por algun Nonomino
+	getOccupieds :: [((Int, Int), Nonomino)] -> [(Int, Int)]
+	getOccupieds list = [ (a+i, b+j) | ((a, b), Nonomino points) <- list,
+												 (i,j) <- points]
+
+
+	-- Devuelve los pares ordenados (i, j) que esten desocupados
+	getEmpties :: [((Int, Int), Nonomino)] -> [(Int, Int)]
+	getEmpties list = [ (i, j) | i <- [0..8],
+										  j <- [0..8],
+										  not (elem (i, j) $ getOccupieds list)]
