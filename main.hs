43ae2a756422dcaf3ff818bdc3145d994eb1cf4b
Hieu Do Ngoc <hieudn.uh@gmail.com>
2019-05-27 19:30:25 -0400

    Implement sudoku printing

--- main.hs
+++ main.hs
@@ -1,19 +1,56 @@
 module Main where
-	
 	import System.Environment
+	import Data.List
 	import Sudoku
 	
 	-- Imprime los elementos de una lista separados linea por linea
-	printline [] = do print("-----------------------")
+	printline [] = do putStrLn "------------------"
 	printline (h:t) = do 
 		putStrLn (show h)
 		printline t
-	
-	main = do
-		printline  board
-		printline  test2
-		
 
+	------------------------------------- SUDOKU PRINTING ------------------------------------------
+
+	sameNonomino (i, j) (u, v) (Sudoku nonos) = 
+		let s = [ Nonomino p | ((x,y), Nonomino p) <- nonos, 
+										find (\(a,b,n) -> (a+x,b+y) == (i,j)) p /= Nothing &&
+										find (\(a,b,n) -> (a+x,b+y) == (u,v)) p /= Nothing ] 
+		in s /= [] 
+
+	sudokuToString sudoku = 
+		let
+			rows = [ getRow i sudoku | i <- [0..8]] 
+			unfold sudoku 9  = []
+			unfold sudoku n = rowToString n (rows !! n) sudoku ++ unfold sudoku (n+1)
+		in "|--------------------------|": unfold sudoku 0
+
+	rowToString n row sudoku = 
+		let
+			rowStr 9 = ""
+			rowStr j = 
+				if sameNonomino (n,j) (n,j+1) sudoku 
+				then " " ++ show (row !! j) ++ " " ++ rowStr (j+1)
+				else " " ++ show (row !! j) ++ "|" ++ rowStr (j+1)
+			separator 9 = ""
+			separator j = 
+				if sameNonomino (n,j) (n+1,j) sudoku 
+				then if j /= 8 then "   " ++ separator (j+1) else "  |" ++ separator (j+1)
+				else if j /= 8 then "---" ++ separator (j+1) else "--|" ++ separator (j+1)
+		in
+			["|" ++ rowStr 0, "|" ++ separator 0]
+
+------------------------------------------ MAIN -----------------------------------------------------
+
+	
+	main = 
+		let 
+			sudoku = head $ buildSudokus testNonos
+			sol = solutionSudoku sudoku
+			board = [ getRow i sol | i <- [0..8]]
+		in 
+			do putStrLn $ unlines $ sudokuToString sudoku
+	
+			
 	testNonos = [  
 		Nonomino [(0,0, 0), (0,1, 0), (0,2, 0), (0,3, 1), (0,4, 0), (1,1, 0), (1,2, 0), (1,3, 0), (2,2, 0)],
 		Nonomino [(0,0, 2), (0,1, 0), (0,2, 3), (0,3, 0), (1,-1, 0), (1,0, 0), (1,1, 0), (1,2, 9), (1,3, 0)],
@@ -27,6 +64,3 @@ module Main where
 		]
 
 						
-	test = head $ buildSudokus testNonos
-	board = [ getRow i (head $ buildSudokus testNonos) | i <- [0..8]]
-	test2 = choices test
